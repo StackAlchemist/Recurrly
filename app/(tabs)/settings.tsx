46e05a01e7e004a131d01fe5b926c511ba83bd1d
StackAlchemist <jolamideprince@gmail.com>
2026-05-14 14:30:48 +0100

    feat: project setup, nativewind, routing and tab navigation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native'
+import { styled } from "nativewind";
+import { SafeAreaView as RNSSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSSafeAreaView); 
+
 const settings = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 export default settings
